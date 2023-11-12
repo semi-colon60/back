ca24cdbc4b34ebaf24591c3f5cdd9959fb4867c3
bashtag <btstsiy@hotmail.com>
2023-11-12 22:29:52 +0300

    GBAY-53 commercial_id DAL methods created

--- DataAccess/Repositories/CommercialIdRepository.cs
+++ DataAccess/Repositories/CommercialIdRepository.cs
@@ -12,6 +12,64 @@ namespace dotnet.DataAccess.Repositories
 			Context = context;
 		}
 
+        public Task AddAsync(CommercialId commercialId)
+        {
+            throw new NotImplementedException();
+        }
 
-	}
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<CommercialId>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<CommercialId>> GetByCommercialTitleAsync(string commercialTitle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<CommercialId>> GetByEmailAsync(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CommercialId> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<CommercialId>> GetByNameAndSurnameAsync(string name, string surname)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<CommercialId>> GetByNameAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<CommercialId>> GetByPhoneAsync(string phone)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<CommercialId>> GetBySurnameAsync(string surname)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CommercialId> GetByUsernameAsync(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(CommercialId commercialId)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
