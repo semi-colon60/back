bb68f280660917b1d1fe148cadc7215ca7a9d4fc
bashtag <btstsiy@hotmail.com>
2023-11-12 20:37:49 +0300

    GBAY-55 accessing data from db to test entites

--- DataAccess/DbContexts/ApplicationContext.cs
+++ DataAccess/DbContexts/ApplicationContext.cs
@@ -1,8 +1,16 @@
+using dotnet.DataAccess.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace dotnet.DataAccess.DbContexts {
 
 	public class ApplicationContext : DbContext {
 		public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) {}
+
+		// DbSet is used to access the database tables.
+		// These are just for testing purposes.
+		public DbSet<CommercialId> CommercialIds { get; set; }
+		public DbSet<Material> Materials { get; set; }
+		public DbSet<MainGroup> MainGroups { get; set; }
+		public DbSet<SubGroup> SubGroups { get; set; }
     }
 }
