87d795899ff1ae1d5a23a55e745e313cc85fcc1c
melk_ <m.seyitoglu2021@gtu.edu.tr>
2023-11-29 18:23:00 +0300

    GBAY-114 Added order model which includes commercial id

---
+++ DataAccess/Models/Order.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace dotnet.DataAccess.Models
+{
+	[Table("order")]
+	public class Order
+	{
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		[Column("order_id")]
+		public Int64 OrderId { get; set; }
+
+		[ForeignKey(nameof(CommercialId))]
+		[Column("commercial_id")]
+		[Required(ErrorMessage = "name is required.")]
+		public Int64 CommercialId { get; set; }
+
+		[Column("order_date")]
+		[MaxLength(11, ErrorMessage ="order date cannot exceed 11 characters.")]
+		public string OrderDate { get; set; } = string.Empty;
+
+		//approval can be an integer??
+		[Column("approval")]
+		[MaxLength(255, ErrorMessage ="approval cannot exceed 255 characters.")]
+		public string Approval { get; set; } = string.Empty;
+
+		// can be an integer??
+		[Column("export_status")]
+		[MaxLength(255, ErrorMessage ="export_status cannot exceed 255 characters.")]
+		public string ExportStatus { get; set; } = string.Empty;
+	}
+}
