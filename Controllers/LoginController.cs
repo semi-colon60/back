48d3d5a2a01db9e0237e8ba408a226d15a372fdb
Salih KARAGÖLLÜ <jdoe@example.com>
2023-11-29 19:04:59 +0300

    GBAY-114 requested changes fixed

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -21,7 +21,7 @@ namespace dotnet.Controllers
 			var result = await _loginService.GetByEmailAsync(email, password);
 			if (result == null)
 				return NotFound("User Not Found!");
-			return Ok(result);
+			return Ok("Login success!");
 		}
 
 		[HttpGet("LoginUsername/{username}")]
@@ -30,7 +30,7 @@ namespace dotnet.Controllers
 			var result = await _loginService.GetByUsernameAsync(username, password);
 			if (result == null)
 				return NotFound("User Not Found!");
-			return Ok(result);
+			return Ok("Login success!");
 		}
 
 	}
