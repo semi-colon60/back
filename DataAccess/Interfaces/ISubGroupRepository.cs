062f8a457bde023f378d87b6daca3dbc25e76783
bashtag <btstsiy@hotmail.com>
2023-12-07 00:52:41 +0300

    GBAY-154 int to long

--- DataAccess/Interfaces/ISubGroupRepository.cs
+++ DataAccess/Interfaces/ISubGroupRepository.cs
@@ -4,14 +4,14 @@ namespace dotnet.DataAccess.Interfaces
 {
 	public interface ISubGroupRepository
 	{
-		Task<SubGroup> GetByIdAsync(int id);
+		Task<SubGroup> GetByIdAsync(long id);
 		Task<IEnumerable<SubGroup>> GetAllAsync();
 		Task AddAsync(SubGroup subGroup);
 		Task UpdateAsync(SubGroup subGroup);
-		Task DeleteAsync(int id);
+		Task DeleteAsync(long id);
 
 		// Additional get methods
 		Task<SubGroup?> GetByNameAsync(string name);
-		Task<IEnumerable<SubGroup>> GetByMainGroupIdAsync(int mainGroupId);
+		Task<IEnumerable<SubGroup>> GetByMainGroupIdAsync(long mainGroupId);
 	}
 }
