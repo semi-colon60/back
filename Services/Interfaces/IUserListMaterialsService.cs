63cb2d6b0a48291a2fa52713b87bd8d2c0a327ed
bashtag <btstsiy@hotmail.com>
2023-12-16 16:13:52 +0300

    GBAY-122 AddToCart and GetTotals methods

--- Services/Interfaces/IUserListMaterialsService.cs
+++ Services/Interfaces/IUserListMaterialsService.cs
@@ -7,7 +7,8 @@ namespace dotnet.Services.Interfaces
 		Task<IEnumerable<Material>> GetAllMaterialsAsync();
 		Task<IEnumerable<Material>> GetMaterialsByMainGroupIdAsync(Int64 id);
 		Task<IEnumerable<Material>> GetMaterialsBySubGroupIdAsync(Int64 id);
-		Task<Material> AddMaterialToCart(Material material);
+		Task<CartItem> AddMaterialToCart(CartItem cartItem);
+		Task<TotalCartValues> GetTotals(Int64 CommercialId);
 		Task<IEnumerable<MainGroup>> GetAllMainGroupsAsync();
 		Task<IEnumerable<SubGroup>> GetSubGroupsByMainGroupId(Int64 id);
 	}
